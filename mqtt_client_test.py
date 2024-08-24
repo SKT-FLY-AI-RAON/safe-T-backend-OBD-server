bf0e451d9b6f6733791ac8d3fcb3dc5268756a2f
BoyoungH <hyong0903@naver.com>
2024-08-24 15:28:24 +0900

    Fix: topic 주소 변경

--- mqtt_client_test.py
+++ mqtt_client_test.py
@@ -6,7 +6,7 @@ import random
 # MQTT 브로커 정보
 broker_address = "3.35.30.20"  # Flask 서버와 동일한 브로커 주소
 port = 1883
-topic = "test/topic"  # 동일한 토픽으로 메시지 전송
+topic = "obd/topic"  # 동일한 토픽으로 메시지 전송
 
 # MQTT 클라이언트 생성
 client = mqtt.Client()
@@ -17,17 +17,23 @@ client.connect(broker_address, port=port)
 # 테스트 데이터를 MQTT로 전송하는 함수
 def send_test_data():
     # 무작위로 OBD 데이터 생성
-    rpm = random.randint(500, 7000)  # RPM은 500에서 7000 사이
-    speed = random.randint(0, 180)   # 속도는 0에서 180 km/h 사이
-    throttle = random.uniform(0, 100)  # 스로틀은 0%에서 100% 사이
+    rpm = random.randint(500, 7000)       # RPM은 500에서 7000 사이
+    speed = random.uniform(0, 180)        # 속도는 0에서 180 km/h 사이
+    load = random.uniform(0, 100)         # Load는 0%에서 100% 사이
+    throttle_pos = random.uniform(0, 100) # 스로틀 포지션은 0%에서 100% 사이
+    pedal_pos = random.uniform(0, 100)    # 페달 포지션은 0%에서 100% 사이
+    fuel_status = random.randint(1, 2)    # FuelStatus는 1 또는 2
     time_info = time.strftime("%Y-%m-%d %H:%M:%S")  # 현재 시간
     
     # 테스트용 OBD 데이터
     obd_data = {
-        'rpm': rpm,                  # 엔진 RPM
-        'speed': speed,              # 속도
-        'throttle': throttle,        # 스로틀 포지션
-        'time': time_info            # 시간 정보
+        "time": time_info,         # 현재 시간 정보
+        "RPM": rpm,                # 엔진 RPM
+        "Speed": speed,            # 속도
+        "Load": load,              # 엔진 부하
+        "ThrottlePos": throttle_pos, # 스로틀 포지션
+        "PedalPos": pedal_pos,     # 페달 포지션
+        "FuelStatus": fuel_status  # 연료 상태
     }
 
     # 데이터를 JSON 형식으로 변환
